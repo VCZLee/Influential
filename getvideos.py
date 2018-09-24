34ad3679f61b2ea68292a53f49f74aa88c111576
Victor Lee <vczlee@gmail.com>
2018-09-24 00:46:34 -0400

    Added loop to fetch all videos in a given user's "uploaded" playlist

--- getvideos.py
+++ getvideos.py
@@ -13,11 +13,11 @@ playlist_parameters = {"key": api_key,
 playlist_query = requests.get(playlist_url, playlist_parameters)
 playlist_results = playlist_query.json()
 playlist_id = playlist_results["items"][0][playlist_part]["relatedPlaylists"]["uploads"]
-print(playlist_id)
+
 
 video_url = "https://www.googleapis.com/youtube/v3/playlistItems?"
 video_part = "snippet"
-max_results = 50
+max_results = 50 #api supports up to 50
 video_parameters = {"key": api_key,
                     "part": video_part,
                     "playlistId": playlist_id,
@@ -27,9 +27,14 @@ video_parameters = {"key": api_key,
 
 video_query = requests.get(video_url, video_parameters)
 video_results = video_query.json()
-print(video_query.url)
+video_list = []
+for i in range(len(video_results["items"])):
+    video_list.append(video_results["items"][i]["snippet"]["title"])
+
+
 while "nextPageToken" in video_results:
     video_parameters["pageToken"] = video_results["nextPageToken"]
     video_query = requests.get(video_url, video_parameters)
     video_results = video_query.json()
-    print(video_query.url)
+    for i in range(len(video_results["items"])):
+        video_list.append(video_results["items"][i]["snippet"]["title"])
