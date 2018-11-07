f4ed73727da5d4ca983c0a2c1cfb8d967018c82a
Victor Lee <vczlee@gmail.com>
2018-11-06 20:26:14 -0500

    added datetime parser

--- getvideos.py
+++ getvideos.py
@@ -1,5 +1,7 @@
 import requests
+import numpy as np
 import pandas as pd
+import datetime
 
 
 def get_playlist_id(channel_id, api_key):
@@ -29,22 +31,30 @@ def get_videos(playlist_id, columns, api_key):
     video_results = video_query.json()
     video_data = []
     for i in range(len(video_results["items"])):
-        video_data.append([video_results["items"][i]["snippet"][column] for column in columns])
+        output = [video_results["items"][i]["snippet"]["resourceId"]["videoId"]]
+        output.extend([video_results["items"][i]["snippet"][column] for column in columns])
+        video_data.append(output)
 
     while "nextPageToken" in video_results:
         video_parameters["pageToken"] = video_results["nextPageToken"]
         video_query = requests.get(video_url, video_parameters)
         video_results = video_query.json()
         for i in range(len(video_results["items"])):
-            video_data.append([video_results["items"][i]["snippet"][column] for column in columns])
-
+            output = [video_results["items"][i]["snippet"]["resourceId"]["videoId"]]
+            output.extend([video_results["items"][i]["snippet"][column] for column in columns])
+            video_data.append(output)
     return video_data
 
+
+publishedAt_parser = np.vectorize(lambda x: datetime.datetime.strptime(x, "%Y-%m-%dT%H:%M:%S.%fZ"))
 
 
 arxiv = "UCNIkB2IeJ-6AmZv7bQ1oBYg" #arXiv
 dunkey = "UCsvn_Po0SmunchJYOWpOxMg" #dunkey
-video_columns = ["title", "description"]
+video_columns = ["title", "description", "publishedAt"]
+dataframe_columns = ["id"] + video_columns
 
-uploads = get_playlist_id(arxiv, api_key)
-video_dataframe = pd.DataFrame(get_videos(uploads, video_columns, api_key), columns=video_columns)
+uploads = get_playlist_id(dunkey, api_key)
+video_dataframe = pd.DataFrame(get_videos(uploads, video_columns, api_key), columns=dataframe_columns)
+
+video_dataframe["publishedAt"]= publishedAt_parser(video_dataframe["publishedAt"])
